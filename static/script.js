dd4d54bd0d0bc9f75559ce889659a37ebaa3f888
blacksempai <benjaminfranclin1998@gmail.com>
2024-01-07 16:49:23 +0200

    Add styling

--- static/script.js
+++ static/script.js
@@ -3,6 +3,22 @@
 async function initProducts() {
     const productsContainerElem = document.getElementById('products_container');
 
-    const products = await fetch('/api/product');
+    const response = await fetch('/api/product');
+    const products = await response.json();
+
+    const productsHtml = products.map(p => `
+        <div class="product_item" id="product_${p.id}">
+            <img class="product_img" width="250" src="${p.photo_url}">
+            <a class="product_name" href="/product?id=${p.id}">${p.name}</a>
+            <div class="product_footer">
+                <p class="product_price"> ${p.price}UAH </p>
+                <button class="product_btn">Add to cart</button>
+            </div>
+        </div>
+    `);
+
+    productsContainerElem.innerHTML = productsHtml.join('');
 
 }
+
+initProducts();
