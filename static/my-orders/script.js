0ab40dfe7fc35ec8a0fc8fe10f813ff8709122be
blacksempai <benjaminfranclin1998@gmail.com>
2024-04-18 19:52:09 +0300

    Fix status in my oreders page

--- static/my-orders/script.js
+++ static/my-orders/script.js
@@ -27,7 +27,7 @@ function getStatusHtml(status){
     switch(status){
         case 'NEW': return '<span class="badge text-bg-success">Новий</span>'
         case 'CANCELED': return '<span class="badge text-bg-danger">Скасований</span>'
-        case 'PENDING': return '<span class="badge text-bg-secondary">Виконаний</span>'
+        case 'DONE': return '<span class="badge text-bg-secondary">Виконаний</span>'
         case 'PROCESS': return '<span class="badge text-bg-warning">В процесі</span>'
         default: return '<span>Статус невідомий</span>'
     }
